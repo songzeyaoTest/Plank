46d214e953ad144e8bb3e2a7df5010ce861030f0
songzeyao <songzeyao@live.com>
2016-01-04 22:20:56 +0800

    Test

--- plank/ViewController.m
+++ plank/ViewController.m
@@ -21,7 +21,8 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated
+    NSLog(@"11");
 }
 
 @end
